54b36bf1eb8f3dbe4c6b1eb14e498edba865bc0c
ofrikamst <ofk2222@gmail.com>
2021-12-29 12:20:30 +0200

    Working on MongoDB

---
+++ src/main/java/io/example/rest/AddressItem.java
@@ -0,0 +1,15 @@
+package io.example.rest;
+
+import javax.persistence.Entity;
+
+@Entity
+public class AddressItem {
+
+    private String state;
+    private String city;
+    private String street;
+    private String number;
+    private String postal;
+
+
+}
